943dece870340917e3449b05e1d27bf64d058e56
Phil Mass and Paige Williams <paige@>
2018-10-30 11:06:11 -0700

    Create constructors for Bank, Users, and Account.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,37 @@
+//Business Logic for Bank
+function Bank (){
+  this.users = [];
+  this.currentUser = null;
+  this.nextUserId = 0;
+}
 
+Bank.prototype.assignUserId = function() {
+  var newId = this.nextUserId;
+  this.nextUserId++;
+  return newId;
+}
+
+Bank.prototype.registerUser = function(username, password){
+  var userId = this.assignUserId();
+  var newUser = new User(userId, username, password);
+  this.currentUser = newUser;
+  this.users.push(newUser);
+}
+
+//Business logic for users
+function User (id, name, password) {
+  this.account = new Account(0);
+  this.id = id;
+  this.name = name;
+  this.password = password;
+}
+
+//Business logic for account
+function Account (balance) {
+  this.balance = balance;
+}
+
+var bank = new Bank();
+
+
+//User Interface Logic
